7a13540b42399c35f0446e8e1f27ddeb1de7913e
antonyvol <antonyvol1@gmail.com>
2016-03-08 19:27:00 +0300

    Add second task

--- Main.java
+++ Main.java
@@ -1,8 +1,15 @@
 public class Main {
 
+
     public enum Direction {
         NORTH, EAST, SOUTH, WEST
     }
+
+    public enum CellState {
+        FREE, OCCUPIED
+    }
+
+
 
     public interface Movable {
         void move(int x, int y);
@@ -10,6 +17,60 @@ public class Main {
 
     public interface Turnable {
         void turnTo(Direction direction);
+    }
+
+
+
+    public class GroundVisorExeption extends RuntimeException {
+        GroundVisorExeption() {
+            super();
+        }
+        GroundVisorExeption(String message) {
+            super(message);
+        }
+    }
+
+    public class GroundCell {
+        private CellState state;
+        private int x;
+        private int y;
+
+        GroundCell() {
+            state = CellState.FREE;
+        }
+    }
+
+    public class Ground {
+        private GroundCell[][] landscape;
+        private int length;
+        private int width;
+
+        Ground(int length, int width) {
+            this.length = length;
+            this.width = width;
+
+            for (int i = 0; i < width; i++) {
+                for (int j = 0; j < length; j++) {
+                    landscape[i][j] = new GroundCell();
+                }
+            }
+        }
+    }
+
+    public class GroundVisor {
+        private Ground ground;
+
+        // not done yet
+        public boolean hasObstacles(int x, int y) {
+            try {
+                if (x > ground.length || y > ground.width) {
+                    throw new GroundVisorExeption();
+                }
+            } catch(GroundVisorExeption gve) {
+                System.err.println(gve.toString() + ": index out of range.");
+            }
+            return true;
+        }
     }
 
     public class Rover implements Movable, Turnable {
